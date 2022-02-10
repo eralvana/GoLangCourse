1040ba97525d969672e20ba55dc7746572cb315c
Shino@TerminalMBA <ernestociencias@yahoo.com.mx>
2022-02-10 17:58:15 -0600

    cuerpo de los correos

--- Creador de Correos/main.go
+++ Creador de Correos/main.go
@@ -24,6 +24,7 @@ func main() {
         if error != nil {
             log.Fatal(error)
         }
-        fmt.Printf("%+v\n", guarda)
+        // fmt.Printf("%+v\n", guarda)
+				fmt.Println("Hola", guarda[3], guarda[1], guarda[2], ":\n \n Espero que estés muy bien.\n \n Aquí te envío los resultados de tus evaluaciones parciales:\n \n Evaluación parcial 1:", guarda[4], "\n Evaluación parcial 2:", guarda[5], "\n Evaluación parcial 3:", guarda[6],"\n Evaluación parcial 4:", guarda[7], "\n \n Promedio:", guarda[8], "\n \n Nosotros, considerando el desempeño que tuviste en el curso, te asignamos la calificación de", guarda[9])
     }
 }
