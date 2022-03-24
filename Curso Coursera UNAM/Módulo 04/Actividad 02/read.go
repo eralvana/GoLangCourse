e6b451036a7ff3c2ff6a440a82d8bdd8166a5230
Shino@TerminalMBA <ernestociencias@yahoo.com.mx>
2022-03-24 11:18:21 -0600

    Actividad 2 del Módulo 4

--- Curso Coursera UNAM/Módulo 04/Actividad 02/read.go
+++ Curso Coursera UNAM/Módulo 04/Actividad 02/read.go
@@ -3,32 +3,52 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"strings"
 )
 
 type Person struct {
-	fname string
-	lname string
+	Fname string
+	Lname string
 }
 
 func main() {
 	fmt.Println("Input the name of the file that have the list of names and press enter:")
 	x := bufio.NewReader(os.Stdin)
-	y, _ := x.ReadString('\n')
-	y = strings.TrimRight(y, "\n")
-	data, error := ioutil.ReadFile(y)
-
-	if error != nil {
-		fmt.Println("There is a problem with the name file or the file")
+	y, erry := x.ReadString('\n')
+	if erry != nil {
+		fmt.Println("There is an error.")
 	}
+	y = strings.TrimRight(y, "\n")
 
-	s := string(data)
-	//first, _ := s.ReadString('\n')
-	//f, _ := first.ReadString(' ')
-	//l, _ := first.ReadString('\n')
-	//p := Person{fname: f, lname: l}
-	//fmt.Println(p)
-	fmt.Println(s)
+	readFile, errRF := os.Open(y)
+	if errRF != nil {
+		fmt.Println("There is an error.")
+	}
+	fileScanner := bufio.NewScanner(readFile)
+
+	fileScanner.Split(bufio.ScanLines)
+
+	slc := make(map[string][]string)
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		wordinaline := strings.Fields(line)
+		slc[line] = []string{wordinaline[0], wordinaline[1]}
+	}
+	readFile.Close()
+
+	var slcstruc []Person
+
+	for _, v := range slc {
+		slcstruc = append(slcstruc, Person{
+			Fname: v[0],
+			Lname: v[1],
+		})
+	}
+	fmt.Println("The slice of structurs is", slcstruc, "and the list is")
+	for k, _ := range slc {
+		fmt.Println(slc[k][0], slc[k][1])
+
+	}
 }
